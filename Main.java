43f81a8abc42f22f0275e5a27660b5a69be6ab0d
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2025-08-06 12:32:56 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,63 @@
+class Main{
+    public static void main(String[] args) {
+        int[] array = {5, 2, 9, 1, 5, 6};
+        quickSort(array, 0, array.length - 1);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+    public static void quickSort(int[] arr, int low, int high){
+        if(low < high){
+            
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public static void quickSort(int[] arr, int low, int high) {
+//     if (low < high) {
+//         int pivotIndex = partition(arr, low, high);
+//         quickSort(arr, low, pivotIndex - 1);
+//         quickSort(arr, pivotIndex + 1, high);
+//     }
+// }
+
+// public static int partition(int[] arr, int low, int high) {
+//     int pivot = arr[high];
+//     int i = low - 1;
+//     for (int j = low; j < high; j++) {
+//         if (arr[j] < pivot) {
+//             i++;
+//             swap(arr, i, j);
+//         }
+//     }
+//     swap(arr, i + 1, high);
+//     return i + 1;
+// }
+
+// public static void swap(int[] arr, int i, int j) {
+//     int temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+// }
